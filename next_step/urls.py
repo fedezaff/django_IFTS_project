e7a4741bfc6cc8b1b3fd0e2046eb4a791721315f
fedezaff <fzaffaina@gmail.com>
2024-09-28 09:38:11 +0200

    add azienda

--- next_step/urls.py
+++ next_step/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('step/', views.step),
     path('offerta/<str:id_offerta>/', views.offerta, name='offerta'),
-
+    path('login/', views.login),
+    path('azienda/<str:id_azienda>/', views.azienda, name='azienda'),
 ]
--- step/views.py
+++ step/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 
 
-
 def step (request):
     Offertes = Offerte.objects.all()
     Azienda = Aziende.objects.all()
@@ -18,8 +17,24 @@ def step (request):
 def offerta (request, id_offerta):
     offerta = get_object_or_404(Offerte, id_offerta=id_offerta)
     competenze = Offerte_competenze.objects.filter(id_offerta=offerta).select_related('id_competenza')
+    Azienda = Aziende.objects.all()
 
     return render(request, 'step/offerta.html' , {
         'offerta':offerta,
         'competenze': competenze,
+        'Azienda':Azienda,
+        })
+
+def login (request):
+    Azienda = Aziende.objects.all()
+
+    return render(request, 'step/login.html' , {
+        'Azienda':Azienda,
+    })
+
+def azienda (request, id_azienda):
+    azienda = get_object_or_404(Aziende, id_azienda=id_azienda)
+
+    return render(request, 'step/azienda.html' , {
+        'azienda':azienda,
         })
